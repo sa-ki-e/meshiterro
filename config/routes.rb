f5cd8f935e2ed249772e4614cab0bc15cb3328d5
Sakie Kawamichi <sakie0303@gmail.com>
2022-02-10 06:52:01 +0000

    [Add]投稿機能の実装

---
+++ config/routes.rb
@@ -0,0 +1,11 @@
+Rails.application.routes.draw do
+root to: "homes#top"
+  devise_for :users
+  
+ resources :post_images, only: [:new, :create, :index, :show]
+  # get 'post_image/new'
+  # get 'post_image/index'
+  # get 'post_image/show'
+get "/homes/about" => "homes#about", as: "about"
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+end
