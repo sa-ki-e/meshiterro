de2d9ab61d6f44f66fe61d12fb7b4b9469b6cd26
Sakie Kawamichi <sakie0303@gmail.com>
2022-02-10 22:29:24 +0000

    [Add]プロフィール画像保存準備

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,14 @@
 Rails.application.routes.draw do
-  get 'users/show'
-  get 'users/edit'
+  #get 'users/show'
+  #get 'users/edit'
+devise_for :users
 root to: "homes#top"
-  devise_for :users
-  
- resources :post_images, only: [:new, :create, :index, :show, :destroy]
+get "/homes/about" => "homes#about", as: "about"
+resources :post_images, only: [:new, :create, :index, :show, :destroy]
+resources :users, only: [:show, :edit]
   # get 'post_image/new'
   # get 'post_image/index'
   # get 'post_image/show'
-get "/homes/about" => "homes#about", as: "about"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
 end
