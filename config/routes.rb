5b40996728660f7edd668c5d43424ee5ae61d294
Sakie Kawamichi <sakie0303@gmail.com>
2022-02-11 01:11:39 +0000

    [Add]2-16/user情報更新機能

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ devise_for :users
 root to: "homes#top"
 get "/homes/about" => "homes#about", as: "about"
 resources :post_images, only: [:new, :create, :index, :show, :destroy]
-resources :users, only: [:show, :edit]
+resources :users, only: [:show, :edit, :update]
   # get 'post_image/new'
   # get 'post_image/index'
   # get 'post_image/show'
